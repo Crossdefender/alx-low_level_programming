6fd5df987e5d8b7bfc969b495f10a2acda242f78
Crossdefender <smartuwalaka@gmail.com>
2022-12-08 21:58:04 -0800

    Variable size c code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
  * main -Entry point
- *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
@@ -13,5 +11,4 @@ int main(void)
 	printf("Sife of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
-
 }
