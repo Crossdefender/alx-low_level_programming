9cabd7ac641963d4b7673b98120ebe12450d6c24
Crossdefender <smartuwalaka@gmail.com>
2022-12-10 08:42:17 -0800

    7-print tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, negative or zero
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
