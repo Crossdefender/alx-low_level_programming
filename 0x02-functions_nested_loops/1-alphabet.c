826c0c7ddc8d939a6e34b70ab447db6b91409387
Crossdefender <smartuwalaka@gmail.com>
2022-12-13 15:46:24 -0800

    1-print alphabet lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Print the alphabet in lower case
+ * return: void
+ */
+
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpa = 'a'; aplha <= 'z'; alpha++)
+		_putchar(alpha);
+
+	_putchar('\n')
+}
