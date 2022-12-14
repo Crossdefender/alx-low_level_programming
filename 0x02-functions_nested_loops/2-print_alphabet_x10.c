2d9b8678e16b73c56ba9c8a19ccd745858e6a99b
Crossdefender <smartuwalaka@gmail.com>
2022-12-13 22:45:14 -0800

    print lower caase 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: Always 0
+ */
+
+void print-alphabet_x10(void)
+{
+	char count, lower;
+
+	for (count = '0'; count <= '9'; count++)
+	{
+		for (lower = 'a'; lower <= 'z'; lower++)
+		{
+			_putchar(lower);
+		}
+		_putchar('\n');
+	}
+}
