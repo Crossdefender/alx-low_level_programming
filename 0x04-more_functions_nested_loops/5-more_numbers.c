0676c1607f81e803a7204c8149ef9513cffe11ca
Crossdefender <smartuwalaka@gmail.com>
2022-12-16 21:04:28 -0800

    Print 10 times numbers from 0 to 9

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,30 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints the numbers 0-14 ten times.
+ * more_numbers - prints 0 - 14
+ *
+ * Return: void
  */
 
 void more_numbers(void)
 {
-	int times, num;
+		char n, c;
+		int i = 0;
 
-	for (times = 0; times <= 9; times++)
-	{
-		for (num = 0; num <= 14; num++)
+		while (i < 10)
 		{
-			if (num > 9)
+			for (n = 0; n <= 14; n++)
 			{
-				_putchar((num/ 10) + '0');
+				c = n;
+				if (n > 9)
+				{
+					_putchar('1');
+					c = n % 10;
+				}
+				_putchar('0' + c);
 			}
-			_putchar((num % 10) + '0');
+
+			_putchar('\n');
+			i++;
 		}
-		_putchar('\n');
-	}
 }
