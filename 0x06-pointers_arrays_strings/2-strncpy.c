b457d31b306935711dd887659a288565862c42b7
Crossdefender <smartuwalaka@gmail.com>
2022-12-21 10:58:07 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,13 @@
  *
  * Return: A pointer to the resulting string dest.
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0, src_len = 0;
 
 	while (src[index++])
-		while (src[index++])
-			src_len++;
-
+		src_len++;
+	
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
 
