341ddf8d1a829eb95a087a86f1f5aed5157ec160
Crossdefender <smartuwalaka@gmail.com>
2022-12-23 07:36:33 -0800

    100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,8 +6,7 @@
  *
  * Return: A pointer to the encoding string.
  */
-
-char *rot13(char *str)
+char*rot13(char *str)
 {
 	int indx1 = 0, indx2;
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
@@ -28,6 +27,7 @@ char *rot13(char *str)
 			     'x', 'y', 'z', 'a', 'b', 'c',
 			     'd', 'e', 'f', 'g', 'h', 'i',
 			     'j', 'k', 'l', 'm'};	 
+	
 	while (str[indx1])
 	{
 		for (indx2 = 0; indx2 < 52; indx2++)
